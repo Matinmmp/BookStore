9d39197d7a72e9c666716ce91300d9026d11d50c
Matinmmp1381 <matinmmp1381@gmail.com>
2023-08-30 12:27:00 -0700

    add search, logo, basket icon to navbar

--- components/MainLayout/Header.tsx
+++ components/MainLayout/Header.tsx
@@ -1,17 +1,32 @@
 import Image from "next/image";
 import Link from "next/link";
+import { AiOutlineSearch } from 'react-icons/ai';
+import { BsBasket } from 'react-icons/bs';
 
 
 const Header = () => {
     return (
         <header className=" fixed top-0 left-0 right-0">
-            <nav className="navbar backdrop-blur-3xl backdrop-saturate-150 bg-background/70">
-                <div className="flex gap-4 w-full" >
+            <nav className="navbar  backdrop-blur-3xl">
+                <div className="flex items-center gap-4 w-full" >
                     <Link href={'/home'} className=" w-16" >
-                        <Image alt="لوگو" src={'/Images/logo2.png'} width={200} height={200} className="w-full h-full"/>
+                        <Image alt="لوگو" src={'/Images/logo2.png'} width={200} height={200} className="w-full h-full" />
                     </Link>
-                    <input type="text" placeholder="جستجو" className="input input-bordered input-group-sm  h-10 w-96"/>
+                    <div className=" flex place-items-center  bg-base-100 rounded-lg">
+                        <AiOutlineSearch className="text-xl w-8" />
+                        <input type="text" placeholder="جستجو" className="input  text-sm h-10 w-[35rem]"
+                            style={{ border: 'none !important', outline: 'none !important' }} />
+                    </div>
+                    <div className="flex-1" />
+                    <div className="flex items-center justify-center gap-2 relative">
+                        <div className="p-2 rounded-md cursor-pointer"
+                            style={{ transform: 'rotateY(180deg)' }}>
+                            <BsBasket className="text-2xl" />                           
+                        </div>
+                        <span className="px-[5px] rounded-[3px] bottom-0 right-0 text-white text-[10px] leading-4 bg-primary absolute">1</span>
+                    </div>
                 </div>
+
             </nav>
         </header>
     )
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="fa" dir='rtl' >
+    <Html lang="fa" dir='rtl' data-theme='winter'>
       <Head />
       <body>
         <Main />
