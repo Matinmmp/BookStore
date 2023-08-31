fb690e6a7926107ddcada3d1582cc5a8e4cc8c19
Matinmmp1381 <matinmmp1381@gmail.com>
2023-08-31 12:27:31 -0700

    complete hero section

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -28,25 +28,29 @@ const Hero = ({ products }: IProps) => {
                     <SwiperSlide className='h-full relative' key={product._id}>
                         <Image alt='sdaf' width={2000} height={1200} className='w-full h-full object-cover'
                             src={`http://localhost:8000/images/products/thumbnails/${product.thumbnail}`} />
+
                         <div className='absolute top-0 right-0 left-0 bottom-0 backdrop-blur-[3px] bg-slate-950 bg-opacity-70'>
-                            <div className='flex flex-col p-16 px-36 mt-[6rem] gap-12'>
+                            <div className='flex flex-col items-center lg:items-start p-4 pt-20 lg:p-16 lg:px-36 lg:mt-[6rem] gap-12'>
                                 <Link href={'/'}>
                                     <h3 className='text-2xl text-white font-semibold'>{product.name}</h3>
                                 </Link>
-                                <div className="flex gap-8">
+                                <div className="flex flex-col items-center lg:flex-row  gap-8">
                                     <Image alt='sdaf' width={2000} height={1300} className='w-[15rem] h-[20rem] object-cover rounded-lg'
                                         src={`http://localhost:8000/images/products/images/${product.images[0]}`} />
 
-                                    <div className="flex flex-col text-white gap-4 w-7/12"
+                                    <div className="hidden lg:flex flex-col text-white gap-4 w-7/12"
                                         dangerouslySetInnerHTML={{ __html: product.description }} />
-                                    <button className="btn btn-primary self-end">
-                                        <Link href={"/"}>
-                                            بیشتر....
-                                        </Link>
-                                    </button>
+                                    <div className="self-end w-full text-center lg:w-auto ">
+                                        <button className="btn btn-primary ">
+                                            <Link href={"/"}>
+                                                بیشتر....
+                                            </Link>
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+
                     </SwiperSlide>
                 )}
             </Swiper>
